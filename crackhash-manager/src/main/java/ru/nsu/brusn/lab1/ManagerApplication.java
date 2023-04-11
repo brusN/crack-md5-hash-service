1afdb7a24bb5f11b08f6467acd8fbe291c6f6459
brusN <mainbrusmail@gmail.com>
2023-04-11 14:23:13 +0700

    Try for fix dependencies for jar

--- crackhash-manager/src/main/java/ru/nsu/brusn/lab1/ManagerApplication.java
+++ crackhash-manager/src/main/java/ru/nsu/brusn/lab1/ManagerApplication.java
@@ -2,9 +2,6 @@ package ru.nsu.brusn.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import ru.nsu.brusn.lab1.model.task.CrackHashTaskManager;
 
 @SpringBootApplication
 public class ManagerApplication {
